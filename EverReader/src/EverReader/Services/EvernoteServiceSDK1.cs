b97d721fd0e42e461e77c43e67676d642593dedb
Simon Kittle <simon@kittle.co.uk>
2015-11-13 16:29:16 +0000

    Implements ability to search for notes, launch reader window

---
+++ EverReader/src/EverReader/Services/EvernoteServiceSDK1.cs
@@ -0,0 +1,53 @@
+﻿using EverReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Evernote.EDAM.NoteStore;
+using Thrift.Transport;
+using Thrift.Protocol;
+using Evernote.EDAM.Type;
+
+namespace EverReader.Services
+{
+    public class EvernoteServiceSDK1 : IEvernoteService
+    {
+        private NoteStore.Client noteStore;
+        private EFDbEvernoteCredentials credentials;
+
+        public EvernoteServiceSDK1(EFDbEvernoteCredentials credentials)
+        {
+            this.credentials = credentials;
+
+            THttpClient noteStoreTransport = new THttpClient(new Uri(credentials.NotebookUrl));
+            TBinaryProtocol noteStoreProtocol = new TBinaryProtocol(noteStoreTransport);
+            noteStore = new NoteStore.Client(noteStoreProtocol);
+        }
+
+        public List<EvernoteNodeMetadataDecorator> GetNotesMetaList(string searchString)
+        {
+            NoteFilter noteFilter = new NoteFilter();
+            noteFilter.Words = searchString;
+
+            NotesMetadataResultSpec resultsSpec = new NotesMetadataResultSpec();
+            resultsSpec.IncludeTitle = true;
+            resultsSpec.IncludeCreated = true;
+            resultsSpec.IncludeNotebookGuid = true;
+            resultsSpec.IncludeUpdated = true;
+            resultsSpec.IncludeAttributes = true;
+            resultsSpec.IncludeTagGuids = true;
+            resultsSpec.IncludeContentLength = true;
+
+            NotesMetadataList noteMetadataList = noteStore.findNotesMetadata(credentials.AuthToken, noteFilter, 0, 100, resultsSpec);
+
+            List<EvernoteNodeMetadataDecorator> notesMetaWrapperList = noteMetadataList.Notes.ConvertAll(noteMeta => new EvernoteNodeMetadataDecorator(noteMeta));
+
+            return notesMetaWrapperList;
+        }
+
+        public Note GetNote(string guid)
+        {           
+            return noteStore.getNote(credentials.AuthToken, guid, true, false, false, false);
+        }
+    }
+}
---
+++ EverReader/src/EverReader/Services/IEvernoteService.cs
@@ -0,0 +1,16 @@
+﻿using Evernote.EDAM.NoteStore;
+using Evernote.EDAM.Type;
+using EverReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EverReader.Services
+{
+    interface IEvernoteService
+    {
+        List<EvernoteNodeMetadataDecorator> GetNotesMetaList(string searchString);
+        Note GetNote(string guid);
+    }
+}
