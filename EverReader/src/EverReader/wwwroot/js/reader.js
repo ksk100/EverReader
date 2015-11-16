b6edd6c3006db8fce0cc7e0d202b7f332545a63c
Simon Kittle <simon@kittle.co.uk>
2015-11-16 16:23:57 +0000

    On Reader page, stops position being saved when moving back up page, unless user stays on page for 5+ seconds.  Closes #23

--- EverReader/src/EverReader/wwwroot/js/reader.js
+++ EverReader/src/EverReader/wwwroot/js/reader.js
@@ -6,6 +6,9 @@
     $(window).scrollTop(targetScrollTop);
 }
 
+var lastSavedScrollTop = 0;
+var lastSavedScrollTopResetTimerSet = 0;
+
 $(document).ready(function () {
 
     // attach the scroll handler
@@ -15,6 +18,7 @@ $(document).ready(function () {
         var savedDocHeight = $(document).height();
         var savedWinHeight = $(window).height();
         var savedWinScrollTop = $(window).scrollTop();
+        var savedTime = moment();
 
         // set timeout for 0.7 seconds
         setTimeout(function () {
@@ -22,24 +26,38 @@ $(document).ready(function () {
             var winHeight = $(window).height();
             var winScrollTop = $(window).scrollTop();
 
-            if (docHeight == savedDocHeight && winHeight == savedWinHeight && winScrollTop == savedWinScrollTop) {
+            // if we are still at the same position (after 0.7 seconds), and position is after last saved, then save the bookmark
+            if (docHeight == savedDocHeight && winHeight == savedWinHeight && winScrollTop == savedWinScrollTop && winScrollTop > lastSavedScrollTop) {
                 // var d = moment().format("h:mm:ss.SS");
                 // console.log("firing save event: " + d);
                 // console.log("docHeight: " + savedDocHeight + ", winHeight: " + savedWinHeight + ", winScrollTop: " + savedWinScrollTop);
-                // TODO: make API call to save position
+
                 var documentScrollPercent = ((savedWinScrollTop) / (savedDocHeight - savedWinHeight)) * 100;
 
                 $.ajax("/api/reading/" + noteGuid, {
                     "method": "PUT",
                     "data": { "percentageRead": documentScrollPercent },
-                    "success": function () {
-                        // TODO: alert user that postion has been saved?
+                    "success" : function () {
+                        lastSavedScrollTop = savedWinScrollTop;
                     },
-                    "error": function () {
+                    "error" : function () {
                         // TODO: alert the user their position wasn't saved?
                     }
                 });
             }
+
+            // if we would save, but the scroll position is higher up the page (user is scrolling up), then set a timeout to reset the lastSavedScrollTop
+            if (docHeight == savedDocHeight && winHeight == savedWinHeight && winScrollTop == savedWinScrollTop &&
+                lastSavedScrollTop > winScrollTop && lastSavedScrollTopResetTimerSet == 0) {
+                // console.log("not saving now, but setting timer to reset scroll position.  saving will resume in 5 seconds");
+                lastSavedScrollTopResetTimerSet = 1;
+                setTimeout(function () {
+                    // console.log("resetting lastSavedScrollTop, so saving will start working again");
+                    lastSavedScrollTop = 0;
+                    lastSavedScrollTopResetTimerSet = 0;
+                }, 5000);
+            }
+
         }, 700);
 
         var documentScrollPercent = ((savedWinScrollTop) / (savedDocHeight - savedWinHeight)) * 100;
